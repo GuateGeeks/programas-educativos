feacfa5d4e1f971743641270c15cdb9ee2e53807
Joaquin Arredondo <joaquinarredondo@MacBook-Air-de-Joaquin.local>
2026-08-03 14:06:05 -0600

    Match homepage and level catalogs to local proposal

---
+++ src/pages/nivel-creador.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import LevelCatalog from '../components/LevelCatalog';
+
+export default function CreatorPage(): React.JSX.Element { return <LevelCatalog levelKey="creador" />; }
---
+++ src/pages/nivel-explorador.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import LevelCatalog from '../components/LevelCatalog';
+
+export default function ExplorerPage(): React.JSX.Element { return <LevelCatalog levelKey="explorador" />; }
